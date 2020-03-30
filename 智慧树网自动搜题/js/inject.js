4ff8451501696bd6f1cb580f3f34c331487a489a
jiajiayao <jiajiayaoabc@qq.com>
2020-03-30 10:13:29 +0800

    优化客户端

--- 智慧树网自动搜题/js/inject.js
+++ 智慧树网自动搜题/js/inject.js
@@ -21,28 +21,35 @@ function getProblem() {
 
     
 
-    //console.log(problems);
+    console.log(problems);
 
-    sendProblem(problems)
+    sendProblem(problems,problems.length)
 
 }
 
 
 //发送问题
-function sendProblem(problems) {
+function sendProblem(problems, len) {
+	
+	let singlePro = [];
+	singlePro.push(problems[len - 1]);
+
+	/*
 	if (isGetting) {
 		tip('已经获取答案请勿重复获取')
 		return
 	}
+	*/
 	isGetting = 1
-	problems = JSON.stringify(problems)
+	singlePro = JSON.stringify(singlePro)
 	tip('开始获取,请等待')
 	isWaitting = 1
 	waitting(0)
     $.ajax({
         type: "get",
         async: true,
-        url: "http://47.93.203.62:8080/getAnswer?problem="+problems,
+		//url: "http://47.93.203.62:8080/getAnswer?problem="+singlePro,
+		url: "http://127.0.0.1:8080/getAnswer?problem="+singlePro,
         dataType: "jsonp",
         jsonp:"jsonpCallback",
         jsonpCallback:"success_jsonpCallback",
@@ -51,9 +58,16 @@ function sendProblem(problems) {
 
             data = json
 
-            for (let i = 0; i < json.length; i++){
-                $($('.subject_type')[i]).html('<h1>答案：'+data[i].answer+'</h1>')
+            
+            $($('.subject_type')[len-1]).html('<h1>答案：'+data[0].answer+'</h1>')
+			
+
+			len--;
+			if (len == -1) {
+				return;
 			}
+			sendProblem(problems,len)
+
 			tip('获取成功')
 			isWaitting = 0
         },
